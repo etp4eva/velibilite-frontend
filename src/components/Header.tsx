df0f5654eca3fd58320167f3cd90c942fbfcc093
etp4eva <eric@ericwebsite.info>
2023-08-17 17:02:28 +0200

    slidey header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Layer, BikeType, DayOfWeek, enumKeys } from '../types/types'
 
 type HeaderProps = {
@@ -12,89 +12,92 @@ type HeaderProps = {
 }
 
 const Header = (props: HeaderProps) => {
-    
+    const [isOpen, setOpen] = useState(true);
+
     return (
-        <header        
-            style={{
-                display: 'flex',
-                flexDirection: 'row',
-                height: '5vh',
-                alignItems: 'center',
-            }}
-        >
-            <div
-                style={{
-                    flexGrow: 2,
-                    paddingLeft: 5,
-                    paddingRight: 5,
-                }}
-            >
-                <select 
-                    name="layer" 
-                    id="layer-select" 
-                    onChange={ (event) => {
-                        const value = event.currentTarget.value;
-                        props.setSelectedLayer(value);
+        <header className={ isOpen ? 'header-open' : 'header-closed' }>
+            <div className='header-content'>
+                <div
+                    style={{
+                        flexGrow: 2,
+                        paddingLeft: 5,
+                        paddingRight: 5,
                     }}
-                    disabled={props.isLoading}
                 >
-                    { 
-                        enumKeys(Layer).map(key => {
-                            const value = Layer[key];
-                            return (<option value={value}>{key}</option>)
-                        }) 
-                    }    
-                </select>
-            </div>
-            <div
-                style={{
-                    flexGrow: 2,
-                    paddingLeft: 5,
-                    paddingRight: 5,
-                }}
-            >
-                <select 
-                    name="bike-type" 
-                    id="bike-type-select"
-                    disabled={props.isLoading || props.selectedLayer == 'stations'}
-                >
-                    {
-                        enumKeys(BikeType).map(key => {
-                            const value = BikeType[key];
-                            return (<option value={value}>{key} bikes</option>)
-                        }) 
-                    }
-                </select>
-            </div>
-            <div
-                style={{
-                    flexGrow: 2,
-                    paddingLeft: 5,
-                    paddingRight: 5,
-                }}
-            >
-                <select 
-                    name="day" 
-                    id="day-select"
-                    onChange={ (event) => {                        
-                        const value = event.currentTarget.value;
-                        props.setSelectedDay(Number(value));
+                    <select 
+                        name="layer" 
+                        id="layer-select" 
+                        onChange={ (event) => {
+                            const value = event.currentTarget.value;
+                            props.setSelectedLayer(value);
+                        }}
+                        disabled={props.isLoading}
+                    >
+                        { 
+                            enumKeys(Layer).map(key => {
+                                const value = Layer[key];
+                                return (<option value={value}>{key}</option>)
+                            }) 
+                        }    
+                    </select>
+                </div>
+                <div
+                    style={{
+                        flexGrow: 2,
+                        paddingLeft: 5,
+                        paddingRight: 5,
                     }}
-                    disabled={ props.isLoading || props.selectedLayer == 'stations' }
-                    defaultValue={ DayOfWeek.Monday }
                 >
-                    {
-                        enumKeys(DayOfWeek).map(key => {
-                            const value = DayOfWeek[key];
-                            /*
-                            if (key == 'All') {
-                                return (<option value={value}>{key} Days</option>)
-                            }*/
-                            return (<option value={value}>{key}</option>)
-                        }) 
-                    }
-                </select>
+                    <select 
+                        name="bike-type" 
+                        id="bike-type-select"
+                        disabled={props.isLoading || props.selectedLayer == 'stations'}
+                    >
+                        {
+                            enumKeys(BikeType).map(key => {
+                                const value = BikeType[key];
+                                return (<option value={value}>{key} bikes</option>)
+                            }) 
+                        }
+                    </select>
+                </div>
+                <div
+                    style={{
+                        flexGrow: 2,
+                        paddingLeft: 5,
+                        paddingRight: 5,
+                    }}
+                >
+                    <select 
+                        name="day" 
+                        id="day-select"
+                        onChange={ (event) => {                        
+                            const value = event.currentTarget.value;
+                            props.setSelectedDay(Number(value));
+                        }}
+                        disabled={ props.isLoading || props.selectedLayer == 'stations' }
+                        defaultValue={ DayOfWeek.Monday }
+                    >
+                        {
+                            enumKeys(DayOfWeek).map(key => {
+                                const value = DayOfWeek[key];
+                                /*
+                                if (key == 'All') {
+                                    return (<option value={value}>{key} Days</option>)
+                                }*/
+                                return (<option value={value}>{key}</option>)
+                            }) 
+                        }
+                    </select>
+                </div>
             </div>
+            <button 
+                className='drawer-button drawer-up'
+                onClick={() => {
+                    if (isOpen) setOpen(false);
+                    else setOpen(true);
+                }}
+            >﹀</button>
         </header>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -41,10 +41,6 @@ const Content = () => {
         })
     }, [])
     
-
-    
-
-
     return (
         <div
             style={{
